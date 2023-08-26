f938b27f749dd9e95842941bfd22a7fe66b411cb
John DoeArtin <artinmevallo@gmail.com>
2023-08-26 17:09:46 +0330

    get player and computer number and some comments

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
+# moudels
+from random import randint
 
+# Welcome and start message
 print("Welcome to the GuessTheNumber game.\n")
 start_inp = input("Start? (y/n): ")
 
+
+# Check start input
 if start_inp == "y":
     
     print("Game started!\n")
+    game = True
+    real_number = randint(0, 100) + 1
+    while game:
+        guessed_number = int(input("your number: "))
 
 
 elif start_inp == "n":
