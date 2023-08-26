5375eecfcc9aef1d5b10a7086e3efd3340bdfbe2
John DoeArtin <artinmevallo@gmail.com>
2023-08-26 17:01:07 +0330

    main.py creation and some codes

---
+++ main.py
@@ -0,0 +1,13 @@
+
+print("Welcome to the GuessTheNumber game.\n")
+start_inp = input("Start? (y/n): ")
+
+if start_inp == "y":
+    
+    print("Game started!\n")
+
+
+elif start_inp == "n":
+    
+    print("bye")
+    quit()
